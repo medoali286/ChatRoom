1706a87aa6d693c671919560f8ca6e87aa76b3f6
currencylayer <medoali286@gmai.com>
2023-02-20 17:03:47 +0200

    final

--- app/src/main/java/com/cst2335/chatroom/ChatRoom.java
+++ app/src/main/java/com/cst2335/chatroom/ChatRoom.java
@@ -2,25 +2,23 @@ package com.cst2335.chatroom;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.cst2335.chatroom.Data.ChatMessage;
 import com.cst2335.chatroom.Data.ChatViewModel;
 import com.cst2335.chatroom.databinding.ActivityChatRoomBinding;
 import com.cst2335.chatroom.databinding.ReceiveMessagesBinding;
 import com.cst2335.chatroom.databinding.SentMessageBinding;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -47,7 +45,7 @@ public class ChatRoom extends AppCompatActivity {
                 chatModel.messages.postValue(messages=new  ArrayList<ChatMessage>());
             }
 
-
+        /*loads buttons / text on screen */
             setContentView(binding.getRoot());
 
 
@@ -61,17 +59,20 @@ public class ChatRoom extends AppCompatActivity {
                 @Override
                 public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-                    if(messages.get(viewType).isSentButton()){
-                        SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater(),
+                    if(messages.get(messages.size()-1).isSentButton()){
+                        //always inflates Sent_message.xml
+                        SentMessageBinding sentMessageBinding = SentMessageBinding.inflate(getLayoutInflater(),
                                 parent, false);
 
-                        View root = binding.getRoot();
+                        View root = sentMessageBinding.getRoot();
                         return new MyRowHolder(root);
 
                     }else {
-                        ReceiveMessagesBinding binding1=ReceiveMessagesBinding.inflate(getLayoutInflater(),
+
+                        //always inflates receive_message.xml
+                        ReceiveMessagesBinding receiveMessagesBinding=ReceiveMessagesBinding.inflate(getLayoutInflater(),
                                 parent,false);
-                        View root = binding1.getRoot();
+                        View root = receiveMessagesBinding.getRoot();
                         return new MyRowHolder(root);
 
                     }
@@ -80,15 +81,16 @@ public class ChatRoom extends AppCompatActivity {
 
 
                 }
-
+                //what are the textViews set to for row POSITION?
                 @Override
                 public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-                    String obj = messages.get(position).getMessage();
-                    holder.messageText.setText(obj);
 
 
-holder.timeText.setText(messages.get(position).getTimeSent());
 
+                        String strMessage = messages.get(position).getMessage();
+                        holder.messageText.setText(strMessage);
+
+                        holder.timeText.setText(messages.get(position).getTimeSent());
 
 
                 }
@@ -98,9 +100,20 @@ holder.timeText.setText(messages.get(position).getTimeSent());
                     return messages.size();
                 }
 
+                //function to check what kind of ChatMessage object is at row position
+                // If the isSend is true, then return 0
+                // so that the onCreateViewHolder checks the viewType and inflates a send_message layout.
+                // If isSend is false, then getItemViewType returns 1 and onCreateViewHolder checks
+                // if the viewType is 1 and inflates a receive_message layout.
+
+
                 @Override
                 public int getItemViewType(int position) {
-                    return position % 2;
+                    if(messages.get(position).isSentButton()) {
+                        return 0;
+                    }else {
+                        return 1;
+                    }
                 }
             });
 
@@ -109,18 +122,13 @@ holder.timeText.setText(messages.get(position).getTimeSent());
 
 
 
+            binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
+
+//the button was clicked, and a boolean true to specify that it was the Sent button that was clicked
+//As for a String representing the time sent
 
 
-
-
-
-
-
-
-
-binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
-
-        binding.sendButton.setOnClickListener(click->{
+            binding.sendButton.setOnClickListener(click->{
 
 
             String text=binding.textInput.getText().toString();
@@ -128,25 +136,63 @@ binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
             String currentDatEndTime = sdf.format(new Date());
 
             messages.add(new ChatMessage(text,currentDatEndTime,true));
+
+
+
+            //redraw the whole list
             myAdapter.notifyItemInserted(messages.size()-1);
-            binding.textInput.setText("");
+
+            binding.textInput.setText("");//now will be empty
+            binding.recycleView.smoothScrollToPosition(messages.size()-1);
 
         });
 
 
 
-
+//the button was clicked, and a boolean false to specify that it was the receive button that was clicked
+//As for a String representing the time receive
 
 
         binding.receiveButton.setOnClickListener(click->{
-ChatMessage chatMessage=messages.get(messages.size()-1);
-            String text=chatMessage.getMessage();
 
-            String currentDatEndTime = chatMessage.getTimeSent();
+
+            String text=binding.textInput.getText().toString();
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
+            String currentDatEndTime = sdf.format(new Date());
 
             messages.add(new ChatMessage(text,currentDatEndTime,false));
+
+
+
+                //redraw the whole list
             myAdapter.notifyItemInserted(messages.size()-1);
-            binding.textInput.setText("");
+
+            binding.textInput.setText("");//now will be empty
+            binding.recycleView.smoothScrollToPosition(messages.size()-1);
+
+
+
+/*            if(messages.size() ==0){
+                messages=new ArrayList<ChatMessage>();
+
+
+            }else {
+                ChatMessage chatMessage = messages.get(messages.size() -1);
+
+                String text = chatMessage.getMessage();
+
+                String currentDatEndTime = chatMessage.getTimeSent();
+
+                messages.add(new ChatMessage(text, currentDatEndTime, false));
+                myAdapter.notifyItemInserted(messages.size() - 1);
+                binding.textInput.setText("");
+                binding.recycleView.smoothScrollToPosition(messages.size()-1);
+
+
+
+            }
+
+ */
 
         });
 
@@ -163,18 +209,20 @@ ChatMessage chatMessage=messages.get(messages.size()-1);
 
 
 
-
+//itemView will the the root of the layout, LinearLayout
 
  class MyRowHolder extends RecyclerView.ViewHolder{
 
         public TextView messageText;
         public  TextView timeText;
 
+
         public MyRowHolder(@NonNull View itemView) {
 
             super(itemView);
             messageText= itemView.findViewById(R.id.message);
             timeText= itemView.findViewById(R.id.time);
+
 
         }
     }
